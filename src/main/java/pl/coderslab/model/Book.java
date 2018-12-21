76b97e8cdb37c83ee57ed3838014ef344a201deb
PJKmiec <pawel@pawelkmiec.pl>
2018-12-21 12:52:37 +0100

    first commit

---
+++ src/main/java/pl/coderslab/model/Book.java
@@ -0,0 +1,15 @@
+package pl.coderslab.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Book {
+    private long id;
+    private String isbn;
+    private String title;
+    private String author;
+    private String publisher;
+    private String type;
+
+
+}
